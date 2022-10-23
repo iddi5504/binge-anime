8cc097bed9aa488db8df5ecb2c6ac416ac3a385f
Iddi <Iddi5504@gmail.com>
2022-10-23 01:01:52 +0100

    new commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const nav = document.getElementById('nav');
+const menuButton = document.getElementsByClassName('menu-button');
+const hamburger = document.getElementsByClassName('hamburger');
 const content = document.getElementById('firstcontent');
-
 const observer = new IntersectionObserver((entry) => {
     entry.forEach(entry => {
         if (!entry.isIntersecting) {
@@ -17,3 +18,18 @@ const observer = new IntersectionObserver((entry) => {
 
 observer.observe(content)
 console.log(content)
+
+menuButton[0].addEventListener('click', (el)=>{
+    if(el.target.attributes[0].value=='false'){
+        el.target.setAttribute("show-hamburger","true")
+        hamburger[0].classList.add("show-hamburger")
+        
+    }else{
+        el.target.setAttribute("show-hamburger","false")
+        hamburger[0].style.transfrom="translateX(-100px)"
+        hamburger[0].classList.remove("show-hamburger")
+
+
+
+    }
+})
